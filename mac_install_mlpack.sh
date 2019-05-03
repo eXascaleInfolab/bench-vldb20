067a3b92e08d2e42bd3819a36d1b0b247fd27b3e
Zakhar Tymchenko <zakhartymchenko@github.com>
2019-05-03 17:30:25 +0200

    changed mac build

--- mac_install_mlpack.sh
+++ mac_install_mlpack.sh
@@ -3,7 +3,7 @@
 git clone https://github.com/mlpack/mlpack mlpack
 mkdir mlpack/build
 cd mlpack/build
-cmake ..
+cmake -D CMAKE_CXX_COMPILER=/usr/local/opt/llvm/clang++ -D CMAKE_C_COMPILER=/usr/local/opt/llvm/clang -D USE_OPENMP=ON -D BUILD_PYTHON_BINDINGS=OFF -D BUILD_TESTS=OFF -D BUILD_CLI_EXECUTABLES=OFF -D CMAKE_CXX_FLAGS= -I =/usr/local/opt/llvm/include -L =/usr/local/opt/llvm/lib ..
 make
 make install
 cd ../..
