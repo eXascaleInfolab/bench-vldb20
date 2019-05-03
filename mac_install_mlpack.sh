c2720cbc5380fe6cb6024258cc4c80328155f99e
Zakhar Tymchenko <zakhartymchenko@github.com>
2019-05-03 17:52:06 +0200

    added parallel compilation for mac build

--- mac_install_mlpack.sh
+++ mac_install_mlpack.sh
@@ -4,7 +4,7 @@ git clone https://github.com/mlpack/mlpack mlpack
 mkdir mlpack/build
 cd mlpack/build
 cmake -D CMAKE_CXX_COMPILER=/usr/local/opt/llvm/clang++ -D CMAKE_C_COMPILER=/usr/local/opt/llvm/clang -D USE_OPENMP=ON -D BUILD_PYTHON_BINDINGS=OFF -D BUILD_TESTS=OFF -D BUILD_CLI_EXECUTABLES=OFF -D CMAKE_CXX_FLAGS= -I =/usr/local/opt/llvm/include -L =/usr/local/opt/llvm/lib ..
-make
+make -j 4
 make install
 cd ../..
 rm -rf mlpack
