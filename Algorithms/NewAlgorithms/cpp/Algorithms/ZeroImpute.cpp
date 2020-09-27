aa1d57ec54455330883d47dcacbd92e71f93cb37
Mourad Khayati <mkhayati@exascale.info>
2020-09-27 16:01:17 +0200

    Update ZeroImpute.cpp

--- Algorithms/NewAlgorithms/cpp/Algorithms/ZeroImpute.cpp
+++ Algorithms/NewAlgorithms/cpp/Algorithms/ZeroImpute.cpp
@@ -4,6 +4,19 @@
 namespace Algorithms
 {
 
+  void ZeroImpute::ZeroImpute_Recovery(arma::mat &input)
+{
+       for (uint64_t j = 0; j < input.n_cols; ++j)
+    {
+        for (uint64_t i = 0; i < input.n_rows; ++i)
+        {
+            if (!arma::is_finite(input(i, j)))
+            {
+                input(i, j) = 0.0;
+            }
+        }
+    } 
+}    
 
 // any other functions go here
 
