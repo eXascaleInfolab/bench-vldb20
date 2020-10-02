6c655bf6b55dc4b6ba88ddf72b0ea1da6b872278
ZakharTymchenko <ZakharTymchenko@github.com>
2020-10-02 17:16:26 +0200

    changed the structure of python version of meanimpute, added a first draft of the guide of how to add a python algorithm

--- NewAlgorithms/python/recovery.py
+++ NewAlgorithms/python/recovery.py
@@ -2,13 +2,7 @@
 import numpy as np
 import time
 
-def meanimpute_rmv(filename_input, filename_output, runtime):
-    # read input matrix
-    matrix = np.loadtxt(filename_input);
-    
-    # beginning of imputation process - start time measurement
-    start_time = time.time();
-    
+def recover_matrix(matrix):
     meanval = [0.0] * len(matrix);
     
     for j in range(0, len(matrix[0])):
@@ -30,20 +24,39 @@ def meanimpute_rmv(filename_input, filename_output, runtime):
         #end for
     #end for
     
+    return matrix;
+
+#end function
+
+
+def rmv_main(algcode, filename_input, filename_output, runtime):
+    # read input matrix
+    matrix = np.loadtxt(filename_input);
+    
+    # beginning of imputation process - start time measurement
+    start_time = time.time();
+    
+    matrix_imputed = recover_matrix(matrix);
+    
     # imputation is complete - stop time measurement
     end_time = time.time()
     
     # calculate the time elapsed in [!] microseconds
     exec_time = (end_time - start_time) * 1000 * 1000
     
-    print "Time (MeanImpute):", exec_time
+    # verification
+    nan_mask = np.isnan(matrix_imputed);
+    matrix_imputed[nan_mask] = np.sqrt(np.finfo('d').max / 100000.0);
     
-    # we choose a binary flag to indicate whether we need runtime results or algorithm output
+    print "Time", algcode, ":", exec_time
+    
+    # we use a binary flag to indicate whether we need runtime results or algorithm output
     if runtime > 0:
-        # if we need runtime we only save one value with the time in microseconds
+        # if we need runtime, we only save one value with the time in microseconds
         np.savetxt(filename_output, np.array([exec_time]))
     else:
         # if we need the output, we instead save the matrix with imputed values to the same file
-        np.savetxt(filename_output, matrix)
+        np.savetxt(filename_output, matrix_imputed)
     # end if
+
 # end function
