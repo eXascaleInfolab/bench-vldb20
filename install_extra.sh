691779c2cf1a88d941a345959926bb96438a0886
Zakhar Tymchenko <TheDeadSkin@hotmail.ch>
2020-12-22 16:58:08 +0100

    Update install_extra.sh

--- install_extra.sh
+++ install_extra.sh
@@ -1,3 +1,8 @@
 pip2 install numpy scipy pandas sklearn
 pip2 install tensorflow
+pip2 install torch torchvision
+pip2 install pandas
+pip2 install ujson
+pip2 install ipdb
+pip2 install scikit-learn
 python linux_build.py
