436cbd48c2834f2279dbed6e5dcb8dea6ae7dad5
Zakhar Tymchenko <TheDeadSkin@hotmail.ch>
2021-11-03 16:12:51 +0100

    Update install_linux.sh

--- install_linux.sh
+++ install_linux.sh
@@ -1,5 +1,7 @@
 sudo apt-get install build-essential cmake libopenmpi-dev libopenblas-dev liblapack-dev libarmadillo-dev libmlpack-dev
-sudo apt-get install octave-pkg-dev r-base gnuplot
+sudo apt-get install octave
+sudo apt-get install liboctave-dev
+sudo apt-get install r-base gnuplot
 sudo apt install python-dev
 curl "https://bootstrap.pypa.io/pip/2.7/get-pip.py" -o get-pip.py
 sudo python get-pip.py
