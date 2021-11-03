061428f8233a5d4448fbd69e723a5963f8e4a5bf
Zakhar Tymchenko <TheDeadSkin@hotmail.ch>
2021-11-03 16:06:16 +0100

    Update install_linux.sh

--- install_linux.sh
+++ install_linux.sh
@@ -1,6 +1,8 @@
 sudo apt-get install build-essential cmake libopenmpi-dev libopenblas-dev liblapack-dev libarmadillo-dev libmlpack-dev
 sudo apt-get install octave-pkg-dev r-base gnuplot
 sudo apt install python-dev
-sudo apt install python-pip
+curl "https://bootstrap.pypa.io/pip/2.7/get-pip.py" -o get-pip.py
+sudo python get-pip.py
+rm get-pip.py
 pip2 install numpy
 python linux_build.py
