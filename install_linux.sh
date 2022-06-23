38b76b134b9b572082359821c8bb33994b914e93
Zakhar Tymchenko <3822929+ZakharTymchenko@users.noreply.github.com>
2022-06-23 09:49:16 +0200

    Update install_linux.sh

--- install_linux.sh
+++ install_linux.sh
@@ -1,8 +1,8 @@
-sudo apt-get install build-essential cmake libopenmpi-dev libopenblas-dev liblapack-dev libarmadillo-dev libmlpack-dev
-sudo apt-get install octave
-sudo apt-get install liboctave-dev
-sudo apt-get install r-base gnuplot
-sudo apt install python-dev
+sudo apt-get install -y build-essential cmake libopenmpi-dev libopenblas-dev liblapack-dev libarmadillo-dev libmlpack-dev
+sudo apt-get install -y octave
+sudo apt-get install -y liboctave-dev
+sudo apt-get install -y r-base gnuplot
+sudo apt-get install -y python-dev
 curl "https://bootstrap.pypa.io/pip/2.7/get-pip.py" -o get-pip.py
 sudo python get-pip.py
 rm get-pip.py
