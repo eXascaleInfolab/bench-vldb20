221ff196d05c9ab00f3e81b7229e6dec19cd0c97
Zakhar Tymchenko <3822929+ZakharTymchenko@users.noreply.github.com>
2024-02-19 11:30:58 +0100

    Update install_extra.sh

--- install_extra.sh
+++ install_extra.sh
@@ -1,10 +1,10 @@
-pip2 install numpy scipy pandas sklearn
+pip2 install numpy scipy pandas
+pip2 install scikit-learn
 pip2 install tensorflow
 pip2 install torch torchvision
 pip2 install pandas
 pip2 install ujson
 pip2 install ipdb
-pip2 install scikit-learn
 pip2 install future
 sudo apt-get install python3-pip
 pip3 install scikit-learn
